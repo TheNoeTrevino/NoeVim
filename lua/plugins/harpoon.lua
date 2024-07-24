b202670bcdce8dc35c5092182b911ef4c9227bff
Noe Trevino <noetrevino.music@gmail.com>
2024-07-24 17:11:03 -0500

    remove harpoon mapping to allow scroll w/C+e

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -31,7 +31,7 @@ return {
         tabline = false,
         tabline_prefix = " ",
         tabline_suffix = " ",
-      }
+      },
     })
 
     -- Setup Telescope extension for Harpoon
@@ -42,29 +42,37 @@ return {
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
 
     vim.keymap.set("n", "<leader>hx", mark.add_file, { desc = "Harpoon: Mark File" })
-    vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end, { desc = "Harpoon: Next Mark" })
-    vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end, { desc = "Harpoon: Previous Mark" })
-    vim.keymap.set("n", "<leader>hm", function() telescope.extensions.harpoon.marks() end, { desc = "Harpoon: Show Marks with Telescope" })
+    vim.keymap.set("n", "<leader>hn", function()
+      ui.nav_next()
+    end, { desc = "Harpoon: Next Mark" })
+    vim.keymap.set("n", "<leader>hp", function()
+      ui.nav_prev()
+    end, { desc = "Harpoon: Previous Mark" })
+    vim.keymap.set("n", "<leader>hm", function()
+      telescope.extensions.harpoon.marks()
+    end, { desc = "Harpoon: Show Marks with Telescope" })
 
     -- Telescope integration
     local conf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-        end
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
 
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-        }):find()
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
     end
 
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(mark.get_all()) end, { desc = "Open Harpoon with Telescope" })
+    -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(mark.get_all()) end, { desc = "Open Harpoon with Telescope" })
   end,
   dependencies = {
     "nvim-lua/plenary.nvim",
