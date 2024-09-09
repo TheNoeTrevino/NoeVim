419fc7c0638be37b07279a5452bd97f835269993
Noe Trevino <noetrevino.music@gmail.com>
2024-09-09 05:44:45 -0500

    add: lspsaga

---
+++ lua/plugins/lspsaga.lua
@@ -0,0 +1,59 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("lspsaga").setup({
+      symbol_in_winbar = {
+        enable = false, -- Disable the breadcrumbs
+      },
+
+      border_style = "round",
+      saga_winblend = 0,
+      move_in_saga = { prev = "<C-p>", next = "<C-n>" },
+      code_action = {
+        keys = {
+          quit = { "q", "<ESC>" },
+        },
+      },
+      rename_action_quit = "<C-c>",
+      definition_preview_icon = "🔍  ",
+      finder_action_keys = {
+        open = "o",
+        vsplit = "s",
+        split = "i",
+        tabe = "t",
+        quit = { "q", "<ESC>" },
+      },
+      hover = {
+        border_style = "round",
+        border = "single",
+        max_width = 0.9,
+        max_height = 0.8,
+        open_link = "<leader>ol",
+        open_cmd = "!explorer",
+        keys = {
+          quit = { "q", "<ESC>" },
+        },
+      },
+      diagnostics = {
+        keys = {
+          quit = { "q", "<ESC>" },
+        },
+      },
+      lightbulb = {
+        enable = false, -- Disable the lightbulb
+      },
+      rename = {
+        in_select = true,
+        auto_save = false,
+        project_max_width = 0.5,
+        project_max_height = 0.5,
+        keys = {
+          quit = { "q", "<ESC>" },
+          exec = "<CR>",
+          select = "x",
+        },
+      },
+    })
+  end,
+}
