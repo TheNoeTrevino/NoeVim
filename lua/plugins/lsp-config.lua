88d5af166123d4c96de4246cb0ca307e9b141705
Noe Trevino <noetrevino.music@gmail.com>
2024-09-09 05:44:19 -0500

    remove: ruff in python

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,17 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = "VeryLazy",
+  opts = {
+    inlay_hints = { enabled = false },
+    servers = {
+      basedpyright = {
+        mason = false,
+        autostart = false,
+      },
+      ruff = {
+        mason = false,
+        autostart = false,
+      },
+    },
+  },
+}
