2a049744d4b992d2368aa5429b6f90ea934fac8d
Noe Trevino <noetrevino.music@gmail.com>
2024-09-24 00:58:23 -0500

    fix: vscode config repetition

--- init.lua
+++ init.lua
@@ -1,24 +1,8 @@
 if vim.g.vscode then
-  vim.api.nvim_set_keymap("n", "j", "h", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "k", "j", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "l", "k", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", ";", "l", { noremap = true, silent = true })
-
-  vim.api.nvim_set_keymap("v", "j", "h", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("v", "k", "j", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("v", "l", "k", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("v", ";", "l", { noremap = true, silent = true })
-
-  vim.api.nvim_set_keymap("x", "j", "h", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("x", "k", "j", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("x", "l", "k", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("x", ";", "l", { noremap = true, silent = true })
-
   vim.api.nvim_set_keymap("o", "j", "h", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("o", "k", "j", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("o", "l", "k", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("o", ";", "l", { noremap = true, silent = true })
-
   vim.api.nvim_set_keymap(
     "n",
     "<esc>",
@@ -37,11 +21,8 @@ if vim.g.vscode then
   vim.api.nvim_set_keymap("n", "<Leader>k", "<Cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "<Leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-
-  -- Map <Leader>gr to get references
   vim.api.nvim_set_keymap("n", "<Leader>gr", "<Cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
-  vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
-  -- Highlight yanked text for a brief time
+  vim.opt.clipboard = "unnamedplus"
   vim.cmd([[
     augroup YankHighlight
       autocmd!
