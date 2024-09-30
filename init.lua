41c37c9da4f205aef868f29238bfdcab7be2e500
TheNoeTrevino <noetrevino.music@gmail.com>
2024-09-30 16:41:09 -0500

    add: markdown mappings

--- init.lua
+++ init.lua
@@ -34,10 +34,13 @@ else
   vim.cmd("colorscheme gruvbox")
   vim.api.nvim_set_hl(0, "Visual", { bg = "#454545", fg = nil })
   vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#ff79c6", bg = nil })
+  vim.api.nvim_set_hl(0, "NoiceCmdlineIcon", { fg = "#ff79c6", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopePromptNormal", { fg = "#f8f8f2", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#6272a4", bg = nil })
+  vim.api.nvim_set_hl(0, "NoiceCmdlinePopupTitleCmdline", { fg = "#ffb86c", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = "#ffb86c", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#6272a4", bg = nil })
+  vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderCmdline", { fg = "#6272a4", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#6272a4", bg = nil })
   vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#f8f8f2", bg = nil, bold = true })
   vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = "#f8f8f2", bg = nil })
@@ -53,6 +56,38 @@ else
   vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#6272a4", bg = nil })
   vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#090909", ctermbg = 0 })
   vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#fcb205", bg = nil })
+
+  vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#91F2EB" })
+
+  -- needed since markodwn removes the highlights
+  vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#fcb500" })
+  vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#fcb500" })
+  vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#fcb500" })
+
+  vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#fc58d6" })
+
+  vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemMenuDefault", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindDefault", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#5bf5ea" })
+  vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#5bf5ea" })
 
   vim.opt.cursorline = true
   vim.opt.cursorlineopt = "number"
