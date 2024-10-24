28103f2ecb646c80dc85017b7ce973f544582e17
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-23 23:35:02 -0500

    improve: neoscroll
    - zz is faster
    - page up and down faster

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -5,11 +5,11 @@ return {
     require("neoscroll").setup({
       -- All these keys will be mapped to their corresponding default scrolling animation
       mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-      hide_cursor = true, -- Hide cursor while scrolling
+      hide_cursor = false, -- Hide cursor while scrolling
       stop_eof = true, -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      easing = "easeOutCubic",
+      -- easing = "easeOutCubic",
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
     })
@@ -19,25 +19,31 @@ return {
     })
     local keymap = {
       ["<C-u>"] = function()
-        neoscroll.ctrl_u({ duration = 130 })
+        neoscroll.ctrl_u({ duration = 150 })
       end,
       ["<C-d>"] = function()
-        neoscroll.ctrl_d({ duration = 130 })
+        neoscroll.ctrl_d({ duration = 150 })
+      end,
+      ["<C-b>"] = function()
+        neoscroll.ctrl_b({ duration = 150 })
+      end,
+      ["<C-f>"] = function()
+        neoscroll.ctrl_f({ duration = 150 })
       end,
       ["<C-y>"] = function()
-        neoscroll.scroll(-0.1, { move_cursor = true, duration = 130 })
+        neoscroll.scroll(-0.1, { move_cursor = true, duration = 100 })
       end,
       ["<C-e>"] = function()
-        neoscroll.scroll(0.1, { move_cursor = true, duration = 130 })
+        neoscroll.scroll(0.1, { move_cursor = true, duration = 100 })
       end,
       ["zt"] = function()
-        neoscroll.zt({ half_win_duration = 130 })
+        neoscroll.zt({ half_win_duration = 100 })
       end,
       ["zz"] = function()
-        neoscroll.zz({ half_win_duration = 130 })
+        neoscroll.zz({ half_win_duration = 150 })
       end,
       ["zb"] = function()
-        neoscroll.zb({ half_win_duration = 130 })
+        neoscroll.zb({ half_win_duration = 100 })
       end,
     }
     local modes = { "n", "v", "x" }
