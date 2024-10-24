4da8852a24aa37faf84369bbeb0c4a7181fccb1a
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-24 04:57:45 -0500

    improve: completions
    - add mappings
    - clean up config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,11 +9,45 @@ return {
     config = function()
       local cmp = require("cmp")
 
+      local noevim_mappings = {
+        -- Confirm
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = false })
+          else
+            fallback()
+          end
+        end, { "i", "c" }),
+      }
+
+      local next_mappings = { "<Down>", "<C-K>", "<C-N>" }
+      for _, m in ipairs(next_mappings) do
+        noevim_mappings[m] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "c" })
+      end
+
+      local previous_mappings = { "<Up>", "<C-L>", "<C-P>" }
+      for _, m in ipairs(previous_mappings) do
+        noevim_mappings[m] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "c" })
+      end
+
       -- Setup for dap completion
       cmp.setup({
         enabled = function()
           return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
         end,
+        mapping = noevim_mappings,
       })
 
       cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
@@ -22,63 +56,15 @@ return {
         },
       })
 
-      -- Setup for `/` command-line
-      cmp.setup.cmdline("/", {
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ select = false })
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-          ["<Down>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-
-          ["<Up>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-        },
+      -- Setup for `/`, `?` command-line
+      cmp.setup.cmdline({ "/", "?" }, {
         sources = {
           { name = "buffer" },
         },
       })
+
       -- Setup for `:` command-line
       cmp.setup.cmdline(":", {
-        -- Make mappings the same as the other completion mappings
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ select = false })
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-          ["<Down>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-
-          ["<Up>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { "i", "c" }),
-        },
         sources = cmp.config.sources({
           { name = "path" },
         }, {
