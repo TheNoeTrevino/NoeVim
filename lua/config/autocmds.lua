e73428635eb15611ed347c47f2ac9aa5e0626ee8
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-28 15:56:10 -0500

    move: future plugin to autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -92,3 +92,22 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
+
+-------------------------------------------------------------------------------
+--                           Markdown Section
+-------------------------------------------------------------------------------
+
+local function capitalize_sentences()
+  local line = vim.api.nvim_get_current_line()
+  line = line:gsub("[%.!?]%s*%l", function(match)
+    return match:upper()
+  end)
+  line = line:gsub("^%l", string.upper)
+  vim.api.nvim_set_current_line(line)
+end
+
+vim.api.nvim_create_autocmd("TextChangedI", {
+  pattern = "*.md",
+  callback = capitalize_sentences,
+  desc = "Auto-capitalize senteces in markdown files",
+})
