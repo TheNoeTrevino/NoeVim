3b6438e35bf2ea45e485977a550898d19ae0f5f4
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-30 22:55:59 -0500

    add: matchup
    -highlights the parenthesis you are currently in

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -57,57 +57,15 @@ return {
     end,
   },
   {
-    "utilyre/sentiment.nvim",
-    version = "*",
-    event = "VeryLazy", -- keep for lazy loading
-    opts = {
-      -- config
-    },
-    init = function()
-      -- `matchparen.vim` needs to be disabled manually in case of lazy loading
-      vim.g.loaded_matchparen = 1
-    end,
-  },
-  {
-    "cbochs/portal.nvim",
+    "andymass/vim-matchup",
     event = "VeryLazy",
     config = function()
-      require("portal").setup({
-        ---@type string[]
-        labels = { "j", "k", "l", ";" },
-        window_options = {
-          relative = "cursor",
-          width = 80,
-          height = 6,
-          col = 2,
-          focusable = false,
-          border = "rounded",
-          noautocmd = true,
-        },
-      })
+      vim.g.matchup_matchparen_enabled = 1
+      vim.g.matchup_matchparen_hi_surround_always = 1
+      vim.g.matchup_delim_nomids = 1
+      vim.g.matchup_delim_noskips = 1
+      vim.g.matchup_matchparen_deferred = 1
+      vim.api.nvim_set_hl(0, "MatchParen", { fg = "#FF9D3C", italic = true })
     end,
-  },
-  {
-    "andrewferrier/debugprint.nvim",
-    opts = {
-      keymaps = {
-        normal = {
-          plain_below = "<leader>xPp",
-          plain_above = "<leader>xPP",
-          variable_below = "<leader> xPv",
-          variable_above = "<leader> xPV",
-          variable_below_alwaysprompt = nil,
-          variable_above_alwaysprompt = nil,
-          textobj_below = "<leader>xPo",
-          textobj_above = "<leader>xPO",
-          toggle_comment_debug_prints = nil,
-          delete_debug_prints = nil,
-        },
-      },
-      commands = {
-        toggle_comment_debug_prints = "ToggleCommentDebugPrints",
-        delete_debug_prints = "DeleteDebugPrints",
-      },
-    },
   },
 }
