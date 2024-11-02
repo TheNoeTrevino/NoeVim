577c2970eac5b12ac4dcf4d22438d71e5bd637ee
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-02 13:08:09 -0500

    add: end and beginning keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -103,10 +103,6 @@ map("n", "<S-CR>", "<cmd>w<cr>")
 -- Better alternate buffer
 map("n", "L", "<C-^>", { noremap = true, silent = true })
 
--- Better end and beginning
-map({ "n", "x", "o" }, "-", "_", { noremap = true, silent = true })
-map({ "n", "x", "o" }, "+", "$", { noremap = true, silent = true })
-
 -- Paste without putting into clipboard
 map("x", "<leader>p", [["_dP]])
 
