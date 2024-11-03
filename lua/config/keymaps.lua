342fb9a8c39167a381a6c5266d42c0b4bf293155
Noe Trevino <noetrevino.music@gmail.com>
2024-11-02 19:42:40 -0500

    chore: make keymappings prettier

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,7 +30,9 @@ local toggle_opts = {
   title_pos = "center",
   ui_width_ratio = 0.5,
 }
+
 local harpoon = require("harpoon")
+
 -------------------------------------------------------------------------------
 --                           Shift Navigation Section
 -------------------------------------------------------------------------------
