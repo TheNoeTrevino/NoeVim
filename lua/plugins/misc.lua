96ba3c5149ac16e042a97daf8bf892af532c42ea
Noe Trevino <noetrevino.music@gmail.com>
2024-11-02 19:37:22 -0500

    move: portal mappings

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -76,4 +76,23 @@ return {
       vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#192020", bg = nil })
     end,
   },
+  {
+    "cbochs/portal.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("portal").setup({
+        ---@type string[]
+        labels = { "j", "k", "l", ";" },
+        window_options = {
+          relative = "cursor",
+          width = 80,
+          height = 6,
+          col = 2,
+          focusable = false,
+          border = "rounded",
+          noautocmd = true,
+        },
+      })
+    end,
+  },
 }
