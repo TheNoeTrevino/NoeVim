90ad6825e899a5d786c6944b4166445091886e44
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-03 01:14:24 -0600

    add: get diff from graph

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -155,11 +155,15 @@ return {
         fields = { "hash", "timestamp", "author", "branch_name", "tag" },
       },
       hooks = {
+        -- Check diff of a commit
         on_select_commit = function(commit)
-          print("selected commit:", commit.hash)
+          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
         end,
+        -- Check diff from commit a -> commit b
         on_select_range_commit = function(from, to)
-          print("selected range:", from.hash, to.hash)
+          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
         end,
       },
     },
