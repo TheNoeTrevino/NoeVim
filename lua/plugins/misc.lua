ae7e7f8bc68ece172bb9b4e71b35bbea56a82dec
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-03 22:00:39 -0600

    ui: changes
    - harpoon
    - portal
    - telescope

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -68,7 +68,7 @@ return {
         window_options = {
           relative = "cursor",
           width = 80,
-          height = 6,
+          height = 5,
           col = 2,
           focusable = false,
           border = "rounded",
