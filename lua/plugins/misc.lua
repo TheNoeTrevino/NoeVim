369e0647fe2dcb43db0d2b6e9200f30da5973205
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-08 21:48:25 -0600

    add: key analyzer

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,7 +5,30 @@ return {
   { "tummetott/unimpaired.nvim", event = "VeryLazy" },
   { "knubie/vim-kitty-navigator", event = "VeryLazy" },
   { "backdround/improved-search.nvim", event = "VeryLazy" },
-  { "Pocco81/HighStr.nvim", event = "VeryLazy" },
+  {
+    "meznaric/key-analyzer.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("key-analyzer").setup({
+        -- Name of the command to use for the plugin
+        command_name = "KeyAnalyzer", -- or nil to disable the command
+
+        -- Customize the highlight groups
+        highlights = {
+          bracket_used = "KeyAnalyzerBracketUsed",
+          letter_used = "KeyAnalyzerLetterUsed",
+          bracket_unused = "KeyAnalyzerBracketUnused",
+          letter_unused = "KeyAnalyzerLetterUnused",
+          promo_highlight = "KeyAnalyzerPromo",
+
+          -- Set to false if you want to define highlights manually
+          define_default_highlights = true,
+        },
+      })
+    end,
+  },
+
+  { "nvchad/volt", lazy = true },
   {
     "meznaric/key-analyzer.nvim",
     event = "VeryLazy",
