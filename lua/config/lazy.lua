9313bf615ed716ed577e0c9cff6debebb1c4b853
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-12 03:58:12 -0600

    fix: breaking changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },
