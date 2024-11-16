cabfe9d770aad5424489bce09e3c57062d48619d
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-16 15:15:26 -0600

    add: timer to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -59,11 +59,21 @@ return {
       lualine_x = {
         {
           function()
-            return os.date("%R")
+            local state = require("timerly.state")
+            if state.progress == 0 then
+              return ""
+            end
+
+            local total = math.max(0, state.total_secs + 1) -- Add 1 to sync with timer display
+            local mins = math.floor(total / 60)
+            local secs = total % 60
+
+            return string.format("%s %02d:%02d", state.mode:gsub("^%l", string.upper), mins, secs)
           end,
-          color = { fg = "#DCD7BA" },
+
+          table.insert(opts.sections.lualine_x, get_timerly_status),
         },
-      }, -- File encoding, format, and type
+      },
       lualine_y = {
         {
           "diff",
