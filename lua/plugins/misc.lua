5b2432f7fd0fa817df3b4a0cc5d61f4ecc05028b
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-16 15:17:11 -0600

    ui: peek definition

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -28,30 +28,6 @@ return {
       })
     end,
   },
-
-  { "nvchad/volt", lazy = true },
-  {
-    "meznaric/key-analyzer.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("key-analyzer").setup({
-        -- Name of the command to use for the plugin
-        command_name = "KeyAnalyzer", -- or nil to disable the command
-
-        -- Customize the highlight groups
-        highlights = {
-          bracket_used = "KeyAnalyzerBracketUsed",
-          letter_used = "KeyAnalyzerLetterUsed",
-          bracket_unused = "KeyAnalyzerBracketUnused",
-          letter_unused = "KeyAnalyzerLetterUnused",
-          promo_highlight = "KeyAnalyzerPromo",
-
-          -- Set to false if you want to define highlights manually
-          define_default_highlights = true,
-        },
-      })
-    end,
-  },
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
