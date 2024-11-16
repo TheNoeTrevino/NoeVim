470ddad0aa43629744709b51f32cb03d6f36718d
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-16 15:15:43 -0600

    reorg: clean up code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   event = "VeryLazy",
   opts = {
-    inlay_hints = { enabled = false },
+    -- inlay_hints = { enabled = false },
+    inlay_hints = {
+      enabled = function(server_name)
+        return server_name ~= "gopls"
+      end,
+    },
     servers = {
       basedpyright = {
         mason = false,
@@ -11,103 +16,6 @@ return {
       ruff = {
         mason = false,
         autostart = false,
-      },
-      vtsls = {
-        -- explicitly add default filetypes, so that we can extend
-        -- them in related extras
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
-        },
-        settings = {
-          complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = true },
-              functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = "literals" },
-              parameterTypes = { enabled = true },
-              propertyDeclarationTypes = { enabled = true },
-              variableTypes = { enabled = false },
-            },
-          },
-        },
-        keys = {
-          {
-            "<leader>ca",
-            function()
-              vim.lsp.buf.code_action()
-            end,
-            desc = "Code Action",
-          },
-          {
-            "gD",
-            function()
-              local params = vim.lsp.util.make_position_params()
-              LazyVim.lsp.execute({
-                command = "typescript.goToSourceDefinition",
-                arguments = { params.textDocument.uri, params.position },
-                open = true,
-              })
-            end,
-            desc = "Goto Source Definition",
-          },
-          {
-            "gR",
-            function()
-              LazyVim.lsp.execute({
-                command = "typescript.findAllFileReferences",
-                arguments = { vim.uri_from_bufnr(0) },
-                open = true,
-              })
-            end,
-            desc = "File References",
-          },
-          {
-            "<leader>co",
-            LazyVim.lsp.action["source.organizeImports"],
-            desc = "Organize Imports",
-          },
-          {
-            "<leader>cM",
-            LazyVim.lsp.action["source.addMissingImports.ts"],
-            desc = "Add missing imports",
-          },
-          {
-            "<leader>cu",
-            LazyVim.lsp.action["source.removeUnused.ts"],
-            desc = "Remove unused imports",
-          },
-          {
-            "<leader>cD",
-            LazyVim.lsp.action["source.fixAll.ts"],
-            desc = "Fix all diagnostics",
-          },
-          {
-            "<leader>cV",
-            function()
-              LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
-            end,
-            desc = "Select TS workspace version",
-          },
-        },
       },
     },
   },
