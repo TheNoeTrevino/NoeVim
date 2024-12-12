eef89a1511e9a619d00a7981970a73586e3da207
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-12 10:51:10 -0600

    remove: markdown formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,21 +1,33 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = "VeryLazy",
-  opts = {
-    -- inlay_hints = { enabled = false },
-    inlay_hints = {
-      enabled = function(server_name)
-        return server_name ~= "gopls"
-      end,
-    },
-    servers = {
-      basedpyright = {
-        mason = false,
-        autostart = false,
+  {
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    opts = {
+      -- inlay_hints = { enabled = false },
+      inlay_hints = {
+        enabled = function(server_name)
+          return server_name ~= "gopls"
+        end,
       },
-      ruff = {
-        mason = false,
-        autostart = false,
+      servers = {
+        basedpyright = {
+          mason = false,
+          autostart = false,
+        },
+        ruff = {
+          mason = false,
+          autostart = false,
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = {},
+        ["markdown.mdx"] = {},
       },
     },
   },
