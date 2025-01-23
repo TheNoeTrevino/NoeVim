f5c855f07505eaa8e1018e546e40de0456f87580
TheNoeTrevino <noetrevino.music@gmail.com>
2025-01-22 18:56:19 -0600

    rm: duplicate sentiment

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -54,16 +54,4 @@ return {
       require("wit").setup({})
     end,
   },
-  {
-    "utilyre/sentiment.nvim",
-    version = "*",
-    event = "VeryLazy", -- keep for lazy loading
-    opts = {
-      -- config
-    },
-    init = function()
-      -- `matchparen.vim` needs to be disabled manually in case of lazy loading
-      vim.g.loaded_matchparen = 1
-    end,
-  },
 }
