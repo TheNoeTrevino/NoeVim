70d96661f2d5f75ee70fad2a277707a1f7edca90
TheNoeTrevino <noetrevino.music@gmail.com>
2025-01-22 18:58:15 -0600

    rm: unused plugins

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,32 +1,30 @@
 return {
   -- Util
-  { "tpope/vim-repeat", event = "VeryLazy" },
   { "ThePrimeagen/vim-be-good", event = "VeryLazy" },
-  { "tummetott/unimpaired.nvim", event = "VeryLazy" },
   { "knubie/vim-kitty-navigator", event = "VeryLazy" },
   { "backdround/improved-search.nvim", event = "VeryLazy" },
-  {
-    "meznaric/key-analyzer.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("key-analyzer").setup({
-        -- Name of the command to use for the plugin
-        command_name = "KeyAnalyzer", -- or nil to disable the command
-
-        -- Customize the highlight groups
-        highlights = {
-          bracket_used = "KeyAnalyzerBracketUsed",
-          letter_used = "KeyAnalyzerLetterUsed",
-          bracket_unused = "KeyAnalyzerBracketUnused",
-          letter_unused = "KeyAnalyzerLetterUnused",
-          promo_highlight = "KeyAnalyzerPromo",
-
-          -- Set to false if you want to define highlights manually
-          define_default_highlights = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "meznaric/key-analyzer.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("key-analyzer").setup({
+  --       -- Name of the command to use for the plugin
+  --       command_name = "KeyAnalyzer", -- or nil to disable the command
+  --
+  --       -- Customize the highlight groups
+  --       highlights = {
+  --         bracket_used = "KeyAnalyzerBracketUsed",
+  --         letter_used = "KeyAnalyzerLetterUsed",
+  --         bracket_unused = "KeyAnalyzerBracketUnused",
+  --         letter_unused = "KeyAnalyzerLetterUnused",
+  --         promo_highlight = "KeyAnalyzerPromo",
+  --
+  --         -- Set to false if you want to define highlights manually
+  --         define_default_highlights = true,
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
