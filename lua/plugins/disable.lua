e576fe98840aadcfc34ba2e5983fea5906ffda7e
TheNoeTrevino <noetrevino.music@gmail.com>
2025-01-22 18:58:45 -0600

    rm: unused auto installed plugins

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -5,4 +5,6 @@ return {
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.ai", enabled = true },
+  { "venv-selector.nvim", enabled = false },
+  { "catppuccin", enabled = false },
 }
