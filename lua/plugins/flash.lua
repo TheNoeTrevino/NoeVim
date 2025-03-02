08725a84afce24207610c758083b01acaade9fa8
TheNoeTrevino <noetrevino.music@gmail.com>
2025-03-02 15:14:40 -0600

    update: flash descriptions

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -6,9 +6,9 @@ return {
   keys = function()
     -- stylua: ignore
     return {
-      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "󱐌 Flash", },
-      { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "󱐌 Flash Treesitter", },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "󱐌 Remote Flash" },
+      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "󱐌 Search", },
+      { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "󱐌 Treesitter Labels", },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "󱐌 Remote" },
       { "R", mode = { "n", "o", "x" }, function() require("flash").treesitter_search() end, desc = "󱐌 Treesitter Search", },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "󱐌 Toggle Flash Search", },
     }
