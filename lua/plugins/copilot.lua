231dd129de2c7e5b050176395e7ebac160111e23
TheNoeTrevino <noetrevino.music@gmail.com>
2025-03-16 04:03:34 -0500

    add: chat mappings

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -15,10 +15,10 @@ return {
         width = 0.8, -- fractional width of parent, or absolute width in columns when > 1
         height = 0.8, -- fractional height of parent, or absolute height in rows when > 1
         relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
-        border = "rounded", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
         row = nil, -- row position of the window, default is centered
         col = nil, -- column position of the window, default is centered
-        title = nil, -- title of chat window
+        title = " Chat - gh for help ", -- title of chat window
         footer = nil, -- footer of chat window
         zindex = 1, -- determines if window is on top or below other floating windows
       },
@@ -34,11 +34,14 @@ return {
         return select.visual(source) or select.buffer(source)
       end,
       require("CopilotChat.integrations.cmp").setup(),
-      vim.keymap.set("n", "<leader>am", "<CMD>CopilotChatModels<CR>"),
     }
   end,
   keys = {
     { "<leader>ae", "<CMD>CopilotChatExplain<CR>", desc = "Explain", mode = { "n", "v" } },
     { "<leader>af", "<CMD>CopilotChatFix<CR>", desc = "Fix", mode = { "n", "v" } },
+    { "<leader>am", "<CMD>CopilotChatModels<CR>", desc = "Models", mode = { "n", "v" } },
+    { "<leader>as", "<CMD>CopilotChatCommit<CR>", desc = "Commit", mode = { "n", "v" } },
+    { "<leader>ac", "<CMD>CopilotChatCommitStaged<CR>", desc = "Commit - Staged", mode = { "n", "v" } },
+    { "<leader>ar", "<CMD>CopilotChatCommitReview<CR>", desc = "Review", mode = { "n", "v" } },
   },
 }
