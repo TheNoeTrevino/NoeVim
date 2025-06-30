c93a1396bf69c6d320b71625d65e7c8ebc04a71b
TheNoeTrevino <noetrevino.music@gmail.com>
2025-06-30 16:37:27 -0500

    enable: linting

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,7 +1,6 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "mfussenegger/nvim-lint", enabled = false },
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.ai", enabled = true },
