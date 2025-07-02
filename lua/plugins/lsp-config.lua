efab97c72c42810051e40add560521b1f5172b1f
TheNoeTrevino <noetrevino.music@gmail.com>
2025-07-01 19:18:58 -0500

    move: formatting out of lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,42 +1,21 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    opts = {
-      inlay_hints = { enabled = false },
-      -- inlay_hints = {
-      --   enabled = function(server_name)
-      --     return server_name ~= "gopls"
-      --   end,
-      -- },
-      servers = {
-        basedpyright = {
-          mason = false,
-          autostart = false,
-        },
-        ruff = {
-          mason = false,
-          autostart = false,
-        },
+  "neovim/nvim-lspconfig",
+  event = "VeryLazy",
+  opts = {
+    inlay_hints = { enabled = false },
+    -- inlay_hints = {
+    --   enabled = function(server_name)
+    --     return server_name ~= "gopls"
+    --   end,
+    -- },
+    servers = {
+      basedpyright = {
+        mason = false,
+        autostart = false,
       },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters = {
-        sqruff = {},
-      },
-      formatters_by_ft = {
-        ["java"] = { "prettier", "google-java-format" },
-        ["xml"] = { "xmlformat" },
-        ["htmlangular"] = { "prettier" },
-        ["cs"] = { "csharpier" },
-        ["typescript"] = { "biome" },
-        ["sql"] = { "sqruff" },
-        ["markdown"] = {},
-        ["markdown.mdx"] = {},
+      ruff = {
+        mason = false,
+        autostart = false,
       },
     },
   },
