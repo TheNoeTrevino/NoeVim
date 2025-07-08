90c3b4a13bd7ac224b3b2f9f88ec84b5422da707
TheNoeTrevino <noetrevino.music@gmail.com>
2025-07-08 03:31:52 -0500

    format: git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -20,45 +20,45 @@ return {
           vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
         end
 
-      -- stylua: ignore start
-      -- Navigation
-      map("n", "]h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "]c", bang = true })
-        else
-          gs.nav_hunk("next")
-        end
-      end, "Next Hunk")
-      map("n", "[h", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "[c", bang = true })
-        else
-          gs.nav_hunk("prev")
-        end
-      end, "Prev Hunk")
-      map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-      map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-      -- Staging
-      map({ "n", "v" }, "<leader>gsh", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      map("n", "<leader>gsb", gs.stage_buffer, "Stage Buffer")
-      -- Stage Undo
-      map("n", "<leader>gsuh", gs.undo_stage_hunk, "Undo Stage Hunk")
-      -- Reset
-      map({ "n", "v" }, "<leader>grh", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>grb", gs.reset_buffer, "Reset Buffer")
-      -- Preview
-      map("n", "<leader>gpi", gs.preview_hunk_inline, "Preview Hunk Inline")
-      map("n", "<leader>gpf", "<cmd>Gitsigns preview_hunk<CR>", "Preview Hunk Float")
-      -- Git blame
-      map("n", "<leader>gbl", function() gs.blame_line({ full = true }) end, "Blame Line")
-      map("n", "<leader>gbt", "<cmd>Gitsigns toggle_current_line_blame<CR>", "Toggle Blame Line")
-      map("n", "<leader>gbb", function() gs.blame() end, "Blame Buffer")
-      -- Git diff
-      map("n", "<leader>gdo", '<cmd>DiffviewOpen<CR>', "Diff Open")
-      map("n", "<leader>gdc", '<cmd>DiffviewClose<CR>', "Diff Close")
+        -- stylua: ignore start
+        -- Navigation
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gs.nav_hunk("next")
+          end
+        end, "Next Hunk")
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gs.nav_hunk("prev")
+          end
+        end, "Prev Hunk")
+        map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
+        map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+        -- Staging
+        map({ "n", "v" }, "<leader>gsh", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map("n", "<leader>gsb", gs.stage_buffer, "Stage Buffer")
+        -- Stage Undo
+        map("n", "<leader>gsuh", gs.undo_stage_hunk, "Undo Stage Hunk")
+        -- Reset
+        map({ "n", "v" }, "<leader>grh", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>grb", gs.reset_buffer, "Reset Buffer")
+        -- Preview
+        map("n", "<leader>gpi", gs.preview_hunk_inline, "Preview Hunk Inline")
+        map("n", "<leader>gpf", "<cmd>Gitsigns preview_hunk<CR>", "Preview Hunk Float")
+        -- Git blame
+        map("n", "<leader>gbl", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>gbt", "<cmd>Gitsigns toggle_current_line_blame<CR>", "Toggle Blame Line")
+        map("n", "<leader>gbb", function() gs.blame() end, "Blame Buffer")
+        -- Git diff
+        map("n", "<leader>gdo", '<cmd>DiffviewOpen<CR>', "Diff Open")
+        map("n", "<leader>gdc", '<cmd>DiffviewClose<CR>', "Diff Close")
 
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-      map("n", "gm", ":MessengerShow<CR>", "Commit Message")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+        map("n", "gm", ":MessengerShow<CR>", "Commit Message")
       end,
     },
   },
