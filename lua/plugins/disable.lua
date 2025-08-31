6c202998e60f5725c1cfa5f103432f32b33fbf58
TheNoeTrevino <noetrevino.music@gmail.com>
2025-08-31 04:44:54 -0500

    chore: clean up

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -3,7 +3,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.ai", enabled = true },
   { "venv-selector.nvim", enabled = false },
   { "catppuccin", enabled = false },
 }
