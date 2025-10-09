bfd8dcda8e3910fe703c0a10f21e12663f77088f
TheNoeTrevino <noetrevino.music@gmail.com>
2025-10-08 21:26:19 -0500

    add back the razor support. Its actually super helpful

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -15,44 +15,97 @@ return {
   -- omnisharp in the lspconfig
   {
     "seblyng/roslyn.nvim",
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {
-      -- your configuration comes here; leave empty for default settings
+    ft = { "cs", "razor" },
+    dependencies = {
+      {
+        -- By loading as a dependencies, we ensure that we are available to set
+        -- the handlers for Roslyn.
+        "tris203/rzls.nvim",
+        config = true,
+      },
     },
+    config = function()
+      local rzls_path = vim.fn.expand("$MASON/share/rzls/libexec")
+
+      local cmd = {
+        "roslyn",
+        "--stdio",
+        "--logLevel=Information",
+        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+        "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+        "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+        "--extension",
+        vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
+      }
+
+      vim.lsp.config("roslyn", {
+        cmd = cmd,
+        handlers = require("rzls.roslyn_handlers"),
+        settings = {
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      })
+      vim.lsp.enable("roslyn")
+    end,
+    init = function()
+      -- We add the Razor file types before the plugin loads.
+      vim.filetype.add({
+        extension = {
+          razor = "razor",
+          cshtml = "razor",
+        },
+      })
+    end,
   },
   { "jlcrochet/vim-razor" },
-  -- {
-  --   "mfussenegger/nvim-dap",
-  --   optional = true,
-  --   opts = function()
-  --     local dap = require("dap")
-  --     if not dap.adapters["netcoredbg"] then
-  --       require("dap").adapters["netcoredbg"] = {
-  --         type = "executable",
-  --         command = vim.fn.exepath("netcoredbg"),
-  --         args = { "--interpreter=vscode" },
-  --         options = {
-  --           detached = false,
-  --         },
-  --       }
-  --     end
-  --     for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
-  --       if not dap.configurations[lang] then
-  --         dap.configurations[lang] = {
-  --           {
-  --             type = "netcoredbg",
-  --             name = "Launch file",
-  --             request = "launch",
-  --             ---@diagnostic disable-next-line: redundant-parameter
-  --             program = function()
-  --               return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-  --             end,
-  --             cwd = "${workspaceFolder}",
-  --           },
-  --         }
-  --       end
-  --     end
-  --   end,
-  -- },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["netcoredbg"] then
+        require("dap").adapters["netcoredbg"] = {
+          type = "executable",
+          command = vim.fn.exepath("netcoredbg"),
+          args = { "--interpreter=vscode" },
+          options = {
+            detached = false,
+          },
+        }
+      end
+      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
+        if not dap.configurations[lang] then
+          dap.configurations[lang] = {
+            {
+              type = "netcoredbg",
+              name = "Launch file",
+              request = "launch",
+              ---@diagnostic disable-next-line: redundant-parameter
+              program = function()
+                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+    end,
+  },
 }
