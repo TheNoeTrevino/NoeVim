8428b38b6e34c9ae4610acd21b831e7756d5890c
Noe Trevino <noetrevino.music@gmail.com>
2025-10-13 23:20:02 -0500

    clean up extras that are now useless

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,8 +34,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
-    -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    -- { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "plugins" },
   },
   checker = {
