f629255233064cc9a072db910aa24b0cd566dc15
TheNoeTrevino <noetrevino.music@gmail.com>
2025-10-30 16:22:17 -0700

    use terminal instead of tmux split

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -57,7 +57,7 @@ return {
         -- window: when run inside a terminal multiplexer, new sessions will be created in a new tab
         -- split: when run inside a terminal multiplexer, new sessions will be created in a new split
         -- NOTE: zellij only supports `terminal`
-        create = "split", ---@type "terminal"|"window"|"split"
+        create = "terminal", ---@type "terminal"|"window"|"split"
         split = {
           vertical = true, -- vertical or horizontal split
           size = 0.3, -- size of the split (0-1 for percentage)
