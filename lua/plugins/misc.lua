8274efbf2feacd522ca93974aea8d79dcdb8c4a4
TheNoeTrevino <noetrevino.music@gmail.com>
2025-10-30 17:39:18 -0700

    clean up

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -28,82 +28,13 @@ return {
     event = "VeryLazy",
     config = function()
       require("wit").setup({})
-      require("mason").setup({
-        ui = {
-          border = "rounded",
-        },
-      })
     end,
   },
-  "tpope/vim-fugitive",
+  { "tpope/vim-fugitive" },
   {
     "vyfor/cord.nvim",
     event = "VeryLazy",
     build = ":Cord update",
     -- opts = {}
-  },
-  -- {
-  --   "Bekaboo/dropbar.nvim",
-  --   event = "VeryLazy",
-  --   -- optional, but required for fuzzy finder support
-  --   dependencies = {
-  --     "nvim-telescope/telescope-fzf-native.nvim",
-  --     build = "make",
-  --   },
-  --   config = function()
-  --     local dropbar_api = require("dropbar.api")
-  --     require("dropbar").setup({
-  --       sources = {
-  --         path = {
-  --           max_depth = 2,
-  --         },
-  --       },
-  --     })
-  --     vim.keymap.set("n", "<Leader>;", dropbar_api.pick)
-  --     vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-  --     vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-  --   end,
-  -- },
-  -- {
-  --   "mizlan/iswap.nvim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("iswap").setup({
-  --       -- Highlight group for the sniping value (asdf etc.)
-  --       -- default 'Search'
-  --       hl_snipe = "ISwapSelection",
-  --
-  --       -- Highlight group for the visual selection of terms
-  --       -- default 'Visual'
-  --       hl_selection = "Visual",
-  --
-  --       -- Automatically swap with only two arguments
-  --       -- default nil
-  --       autoswap = true,
-  --     })
-  --   end,
-  -- },
-  {
-    "shortcuts/no-neck-pain.nvim",
-    event = "VeryLazy",
-    version = "*",
-    config = function()
-      require("no-neck-pain").setup({
-        width = 80,
-        buffers = {
-          right = {
-            enabled = false,
-          },
-          scratchPad = {
-            enabled = true,
-            fileName = "neckPain",
-            location = "~/notes/neckpain",
-          },
-          bo = {
-            filetype = "md",
-          },
-        },
-      })
-    end,
   },
 }
