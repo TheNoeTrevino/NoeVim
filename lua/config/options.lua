d74f45382c688b6dd4e50dc06f4ec74b222f5fba
Noe Trevino <noe.trevino@respec.com>
2025-11-17 13:41:22 -0600

    treesitter > lsp semantic tokens

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -44,7 +44,7 @@ vim.o.cmdheight = 0
 
 vim.o.winborder = "single"
 
-vim.highlight.priorities.semantic_tokens = 205 -- default is 125
+vim.highlight.priorities.semantic_tokens = 195 -- default is 125
 vim.highlight.priorities.treesitter = 200 -- default is 100
 
 vim.o.exrc = true
