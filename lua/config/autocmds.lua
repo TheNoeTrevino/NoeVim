8c025145a89190a3c89b96fa353df549fd0fc79b
Noe Trevino <noetrevino.music@gmail.com>
2026-01-18 02:29:36 +0000

    clean up autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,13 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set("t", "<localleader>t", "<cmd>:ToggleTerm<CR>", opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+local map = LazyVim.safe_keymap_set
 
 -- save buffers in markdown when leaving then
 vim.api.nvim_create_autocmd("FileType", { pattern = "markdown", command = "set awa" })
@@ -28,130 +22,6 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
-
-local function setup_highlight_for_gruvbox()
-  if vim.g.colors_name == "gruvbox" then
-    vim.api.nvim_set_hl(0, "String", { fg = "#89b55b" })
-    vim.api.nvim_set_hl(0, "Search", { fg = "#2E3436", bg = "#A8B665" })
-    vim.api.nvim_set_hl(0, "CurSearch", { fg = "#FFFFFF", bg = "#EA6962" })
-    -- vim.api.nvim_set_hl(0, "Identifier", { fg = "#89B482" })
-  end
-end
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "gruvbox",
-  callback = function()
-    setup_highlight_for_gruvbox()
-  end,
-})
-
-setup_highlight_for_gruvbox()
-
-local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "moonfly",
-  callback = function()
-    vim.api.nvim_set_hl(0, "Boolean", { fg = "#849e5d", bold = false })
-  end,
-  group = custom_highlight,
-})
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "GitConflictDetected",
-  callback = function()
-    vim.notify("Conflict detected in: " .. vim.fn.expand("%:t") .. " ")
-
-    -- Schedule Telescope to run after the current event loop
-    vim.schedule(function()
-      vim.cmd("GitConflictListQf")
-      vim.cmd("cclose")
-    end)
-  end,
-})
-
-local map = LazyVim.safe_keymap_set
-
-vim.api.nvim_create_user_command("NormMap", function()
-  vim.keymap.del("n", "h")
-  vim.keymap.del("n", "j")
-  vim.keymap.del("n", "l")
-  vim.keymap.del("n", ";")
-
-  map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-  map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
-  -- Remap j; for navigation/o-pending
-  map({ "n", "x", "o" }, "h", "<Left>", { desc = "Right", silent = true })
-  map("o", "j", "<Down>", { desc = "Down", silent = true })
-  map("o", "k", "<Up>", { desc = "Up", silent = true })
-  map({ "n", "x", "o" }, "l", "<Right>", { desc = "Left", silent = true })
-
-  -- Move Lines
-  map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-  map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-  map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-  map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-  map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
-  map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-
-  -- Terminal Mappings
-  map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window" })
-  map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window" })
-  map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window" })
-  map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to Right Window" })
-
-  -- Use vim-tmux-navigator if in tmux, otherwise use kitty navigator
-  if vim.env.TMUX then
-    map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true })
-    map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true })
-    map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true })
-    map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true })
-  else
-    map("n", "<C-h>", ":KittyNavigateLeft<CR>", { noremap = true, silent = true })
-    map("n", "<C-j>", ":KittyNavigateDown<CR>", { noremap = true, silent = true })
-    map("n", "<C-k>", ":KittyNavigateUp<CR>", { noremap = true, silent = true })
-    map("n", "<C-l>", ":KittyNavigateRight<CR>", { noremap = true, silent = true })
-  end
-end, {})
-
-vim.api.nvim_create_user_command("NoeMap", function()
-  map({ "n", "x" }, "k", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-  map({ "n", "x" }, "l", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
-  -- Remap j; for navigation/o-pending
-  map({ "n", "x", "o" }, "j", "<Left>", { desc = "Right", silent = true })
-  map("o", "k", "<Down>", { desc = "Down", silent = true })
-  map("o", "l", "<Up>", { desc = "Up", silent = true })
-  map({ "n", "x", "o" }, ";", "<Right>", { desc = "Left", silent = true })
-
-  -- Move Lines
-  map("n", "<A-l>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-  map("i", "<A-l>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-  map("n", "<A-k>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-  map("i", "<A-k>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-  map("v", "<A-l>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
-  map("v", "<A-k>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-
-  -- Terminal Mappings
-  map("t", "<C-j>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window" })
-  map("t", "<C-k>", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window" })
-  map("t", "<C-l>", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window" })
-  map("t", "<C-;>", "<cmd>wincmd l<cr>", { desc = "Go to Right Window" })
-
-  -- Use vim-tmux-navigator if in tmux, otherwise use kitty navigator
-  if vim.env.TMUX then
-    map("n", "<C-j>", "<cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true })
-    map("n", "<C-k>", "<cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true })
-    map("n", "<C-l>", "<cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true })
-    map("n", "<C-;>", "<cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true })
-  else
-    map("n", "<C-j>", ":KittyNavigateLeft<CR>", { noremap = true, silent = true })
-    map("n", "<C-k>", ":KittyNavigateDown<CR>", { noremap = true, silent = true })
-    map("n", "<C-l>", ":KittyNavigateUp<CR>", { noremap = true, silent = true })
-    map("n", "<C-;>", ":KittyNavigateRight<CR>", { noremap = true, silent = true })
-  end
-end, {})
 
 vim.api.nvim_create_user_command("ToggleGutter", function()
   if vim.wo.number or vim.wo.relativenumber or vim.wo.signcolumn ~= "no" or vim.wo.foldcolumn ~= "0" then
@@ -200,28 +70,12 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    -- let o make a new line
-    vim.opt.formatoptions:remove({ "o" })
-  end,
-  desc = "Disable New Line Comment",
-})
-
-vim.api.nvim_create_autocmd("FileType", {
   pattern = { "dap-repl", "dapui_watches", "dapui_hover" },
   callback = function()
     vim.b.completion = true
   end,
   desc = "Enable completion for DAP-REPL filetypes",
 })
-
-vim.api.nvim_create_user_command("Diff", function()
-  -- let code diff do this for us
-  vim.cmd("Gitsigns toggle_word_diff false")
-  vim.cmd("Gitsigns toggle_linehl false")
-  vim.cmd("CodeDiff")
-end, {})
 
 -- AUtocmd to boost certain LSP semantic token highlight priority
 -- readonly is boosted so show constants
@@ -298,21 +152,15 @@ vim.api.nvim_create_autocmd("LspTokenUpdate", {
   end,
 })
 
--- todo: _help_
--- NOTE: (grammer, @this)
--- `hello`
--- Author: hello
--- FIXME: ljljljlj
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "TSUpdate",
---   callback = function()
---     require("nvim-treesitter.parsers").comment = {
---       install_info = {
---         url = "https://github.com/OXY2DEV/tree-sitter-comment",
---
---         branch = "main", -- only needed if different from default branch
---         queries = "queries/",
---       },
---     }
---   end,
--- })
+-- force o and O to show completion menu
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    -- Schedule to run after blink's own InsertEnter handler
+    vim.schedule(function()
+      local mode = vim.api.nvim_get_mode().mode
+      if mode:match("i") then
+        require("blink.cmp").show()
+      end
+    end)
+  end,
+})
