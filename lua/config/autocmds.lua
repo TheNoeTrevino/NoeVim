e4d2a23751433e7714096fd316c9ae4789def3a5
Noe Trevino <noetrevino.music@gmail.com>
2026-06-24 21:17:07 -0700

    config: add default autocmds
    Baseline autocmds (highlight-yank, last-loc, close-with-q, resize-splits, auto-create-dir,
    etc.) in config/lazyvim-autocmds.lua, required from autocmds.lua.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Vendored LazyVim default autocmds (loaded first, mirrors LazyVim load order).
+require("config.lazyvim-autocmds")
 -- Add any additional autocmds here
 
 -- save buffers in markdown when leaving then
