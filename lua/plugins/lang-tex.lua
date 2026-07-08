a30ac3d41c69020ee77110652dd25f6a95e10f74
Noe Trevino <noetrevino.music@gmail.com>
2026-07-07 22:46:12 -0700

    add: latex extension

---
+++ lua/plugins/lang-tex.lua
@@ -0,0 +1,47 @@
+return {
+  -- Add BibTeX & LaTeX to treesitter, but let vimtex own LaTeX highlighting
+  -- (treesitter's latex highlight is disabled to avoid conflicts).
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.highlight = opts.highlight or {}
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "bibtex", "latex" })
+      end
+      if type(opts.highlight.disable) == "table" then
+        vim.list_extend(opts.highlight.disable, { "latex" })
+      else
+        opts.highlight.disable = { "latex" }
+      end
+    end,
+  },
+
+  {
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading breaks inverse search
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      -- Use Zathura for the PDF viewer with SyncTeX forward/inverse search
+      -- (otherwise VimTeX falls back to xdg-open, which opens a browser).
+      vim.g.vimtex_view_method = "zathura"
+    end,
+    keys = {
+      { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        texlab = {
+          keys = {
+            { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
+          },
+        },
+      },
+    },
+  },
+}
