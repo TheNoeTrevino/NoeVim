01f59bcf3654838c365ed7cc7ee08098afc1e779
Noe Trevino <noe.trevino@respec.com>
2026-07-13 08:22:50 -0700

    csharp: razor/rosalyn for nmcris

--- lua/plugins/lang-dotnet.lua
+++ lua/plugins/lang-dotnet.lua
@@ -1,6 +1,4 @@
-local Util = require("util")
 return {
-  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "c_sharp", "fsharp" } },
@@ -34,27 +32,9 @@ return {
     opts = {
       servers = {
         fsautocomplete = {},
-        omnisharp = {
-          handlers = {
-            ["textDocument/definition"] = function(...)
-              return require("omnisharp_extended").handler(...)
-            end,
-          },
-          keys = {
-            {
-              "gd",
-              Util.has("telescope.nvim") and function()
-                require("omnisharp_extended").telescope_lsp_definitions()
-              end or function()
-                require("omnisharp_extended").lsp_definitions()
-              end,
-              desc = "Goto Definition",
-            },
-          },
-          enable_roslyn_analyzers = true,
-          organize_imports_on_format = true,
-          enable_import_completion = true,
-        },
+        -- Disabled: replaced by roslyn.nvim (see csharp.lua). Running OmniSharp
+        -- alongside Roslyn causes duplicate diagnostics/completions on .cs.
+        omnisharp = { enabled = false },
       },
     },
   },
