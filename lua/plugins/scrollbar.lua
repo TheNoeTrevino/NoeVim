b568b3b9fecf93372c70a877ba795238a381e069
Noe Trevino <noe.trevino@respec.com>
2026-07-30 11:44:32 -0700

    feat: exlude filetypes from scrollbar nvim

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,3 +1,26 @@
 return {
-  { "petertriho/nvim-scrollbar", event = "VeryLazy", opts = {} },
+  "petertriho/nvim-scrollbar",
+  event = "VeryLazy",
+  opts = {
+    excluded_filetypes = {
+      -- defaults (must be repeated: opts are merged by list index)
+      "blink-cmp-menu",
+      "dropbar_menu",
+      "dropbar_menu_fzf",
+      "DressingInput",
+      "dbui",
+      "cmp_docs",
+      "cmp_menu",
+      "noice",
+      "prompt",
+      "TelescopePrompt",
+      -- extras
+      "OverseerList",
+      "snacks_input",
+      "snacks_picker_input",
+      "snacks_picker_list",
+      "snacks_notif",
+      "snacks_dashboard",
+    },
+  },
 }
