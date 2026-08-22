dafd6a3501227033da8c5701bfc3bfa34da13341
Noe Trevino <noetrevino.music@gmail.com>
2026-08-22 02:41:20 -0700

    migrate: remove disable entries
    we have full control over the config now

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,11 +1,1 @@
-return {
-  { "nvim-mini/mini.pairs", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "MagicDuck/grug-far.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "catppuccin", enabled = false },
-  { "omnisharp-extended-lsp.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
-  -- I don't use this much and it has a lot of dependencies.
-  { "iamcco/markdown-preview.nvim", enabled = false },
-}
+return {}
