892b2dde465b8cd746bb11b8212e8892677391b6
Noe Trevino <noetrevino.music@gmail.com>
2026-08-22 02:37:39 -0700

    py: reenable venv selector

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -3,7 +3,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "MagicDuck/grug-far.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "venv-selector.nvim", enabled = false },
   { "catppuccin", enabled = false },
   { "omnisharp-extended-lsp.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
